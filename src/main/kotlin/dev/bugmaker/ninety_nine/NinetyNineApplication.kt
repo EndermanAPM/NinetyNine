9698d75b00d7f7998ec0b85183ccceed60de3912
Xavier Martínez <xavier1997@gmail.com>
2023-01-27 11:44:46 +0100

    This is not the commit message you are looking for

---
+++ src/main/kotlin/dev/bugmaker/ninety_nine/NinetyNineApplication.kt
@@ -0,0 +1,11 @@
+package dev.bugmaker.ninety_nine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NinetyNineApplication
+
+fun main(args: Array<String>) {
+	runApplication<NinetyNineApplication>(*args)
+}
---
+++ src/test/kotlin/dev/bugmaker/ninety_nine/NinetyNineApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.bugmaker.ninety_nine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NinetyNineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
